e995a4b4933e2a45a79e91fd4e9bdf77828537e1
ollewarne <209520777+ollewarne@users.noreply.github.com>
2025-10-30 15:07:55 +0100

    update to add timer that counts down from 30

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const score = document.getElementById("score");
+const countDown = document.getElementById("timer");
 const grid = document.getElementById("grid");
+const startButton = document.getElementById("start-button");
 let scoreCount = 0;
+let roundTimer = 30;
+let boxInterval;
 
 
-grid.addEventListener('click', (event) => {
+grid.addEventListener('click', function handleEvent(event) {
     if (event.target.classList.contains("item")) {
         if (event.target.style.backgroundColor === "green") {
             scoreCount++
@@ -13,15 +17,26 @@ grid.addEventListener('click', (event) => {
     }
 })
 
-const boxTimer = setInterval(changeColor, 1000)
+startButton.addEventListener('click', () => {
+    boxInterval = setInterval(changeColor, 1000);
+    startButton.style.display = "none";
+});
 
 function changeColor() {
     let randIndex = Math.floor(Math.random() * grid.children.length)
     let item = [...grid.children][randIndex]
     item.style.backgroundColor = "green";
     setTimeout(changeBackColor, 500, item)
+    roundTimer--
+    countDown.textContent = roundTimer;
+    if (roundTimer === 0) stopGame();
 }
 
 function changeBackColor(element) {
     element.style.backgroundColor = "red";
+}
+
+function stopGame() {
+    clearInterval(boxInterval);
+    grid.removeEventListener(handleEvent);
 }
